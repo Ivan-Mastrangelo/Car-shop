f1e533a73a6cacbac300696473b91533063089c9
Ivan Mastrangelo Dias <masttrangello@gmail.com>
2022-07-07 16:22:56 -0300

    refactor: alterar mode de exportação dos elementos da interface Vehicle

--- src/interfaces/VehicleInterface.ts
+++ src/interfaces/VehicleInterface.ts
@@ -1,6 +1,6 @@
 import { z } from 'zod';
 
-const VehicleSchema = z.object({
+export const VehicleSchema = z.object({
   model: z.string().min(3),
   year: z.number().gte(1900).lte(2022),
   color: z.string().min(3),
@@ -8,6 +8,4 @@ const VehicleSchema = z.object({
   buyValue: z.number().int(),
 });
 
-type Vehicle = z.infer<typeof VehicleSchema>;
-
-export { VehicleSchema, Vehicle };
+export type Vehicle = z.infer<typeof VehicleSchema>;
