2a7068a17a6e39ff8e9e3ba2def51ef1b1384241
Ivan Mastrangelo Dias <masttrangello@gmail.com>
2022-07-12 13:47:52 -0300

    refactor: inserir id opcional em carSchema para utilizà-lo nos testes

--- src/interfaces/CarInterface.ts
+++ src/interfaces/CarInterface.ts
@@ -4,6 +4,7 @@ import { VehicleSchema } from './VehicleInterface';
 export const CarSchema = VehicleSchema.extend({
   doorsQty: z.number().gte(2).lte(4),
   seatsQty: z.number().gte(2).lte(7),
+  _id: z.string().optional(),
 });
 
 export type Car = z.infer<typeof CarSchema>;
